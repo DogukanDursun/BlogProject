eafa04a297814bbb25d9c74e6ed6ce407b5ceca6
DogukanDursun <dod63y@gmail.com>
2023-05-03 18:59:04 +0300

    BlogProject

---
+++ BlogProject/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using BlogProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProject.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        private readonly BlogContext _context;
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger, BlogContext context)
+        {    
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Post(int Id)
+        {
+            var blog = _context.Blog.Find(Id);
+            blog.Author = _context.Author.Find(blog.AuthorId);
+            blog.ImagePath = "/img/" + blog.ImagePath;
+            return View(blog);
+        }
+
+        public IActionResult Index(string search)
+        {
+            var list = _context.Blog.Take(4).Where(b => b.IsPublish).OrderByDescending(x => x.CreateTime).ToList();
+            if(!string.IsNullOrEmpty(search))
+            {
+                list = list.Where(x => x.Title.Contains(search)).ToList();
+            }
+            foreach (var blog in list)
+            {
+                blog.Author = _context.Author.Find(blog.AuthorId);
+            }
+            return View(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
